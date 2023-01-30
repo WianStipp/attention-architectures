c1bd5c488e2c0b9fc9307a833cac684aa3f85e0d
Wian Stipp <stippwian@gmail.com>
2023-01-29 23:00:59 -0800

    added todo

--- transfomer/README.md
+++ transfomer/README.md
@@ -1,1 +1,8 @@
 Vaswani et al. https://arxiv.org/pdf/1706.03762.pdf
+
+To do:
+
+- [ ] positional encoding
+- [ ] attention mask
+- [ ] decoder blocks
+- [ ] training script
--- transfomer/transfomer/positional_encoding.py
+++ transfomer/transfomer/positional_encoding.py
@@ -9,10 +9,8 @@ class PositionalEncoding(nn.Module):
   def __init__(self, d_model: int) -> None:
     super().__init__()
     pe = T.zeros(MAX_SEQ_LENGTH, d_model)
-    # half implemented!
     self.register_buffer('pe', self.pe)
 
   def forward(self, encoding: T.Tensor) -> T.Tensor:
     """Position encode the input encoding."""
     return encoding + Variable(self.pe[:, :encoding.size(1)], requires_grad=False)
-
