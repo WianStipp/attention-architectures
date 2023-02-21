055bbddb2010d3d12b3438528214e4ef5425536c
Wian Stipp <stippwian@gmail.com>
2023-02-21 14:22:46 -0800

    built out some more of the training script

--- gpt/gpt/modeling.py
+++ gpt/gpt/modeling.py
@@ -15,6 +15,7 @@ class GPT(nn.Module):
     self.config = config
     self.lut = nn.Embedding(self.config.vocab_size, self.config.d_model)
     self.decoder_blocks = nn.ModuleList([GPTDecoderBlock.from_gptconfig(self.config) for _ in range(config.n_decoder_blocks)])
+    # TODO: add positional encoding
     self.positional_encoder = lambda x: x
 
   def forward(self, tokens: T.Tensor, padding_mask: Optional[T.Tensor] = None):
@@ -23,11 +24,11 @@ class GPT(nn.Module):
     Args:
       tokens: (batch_size, sequence_length) shape of tokens.
       padding_mask: Padding mask (causal mask will be applied internally)"""
-
     embeddings = self.lut(tokens)
     embeddings += self.positional_encoder(embeddings)
     for block in self.decoder_blocks:
       embeddings = block(embeddings, padding_mask)
+    return None, (embeddings**2).mean()
     #TODO: next token prediction head
 
 
@@ -36,14 +37,27 @@ class GPTDecoderBlock(nn.Module):
   def __init__(self, d_model: int, d_k: int, d_v: int, n_heads: int, dim_feedfwd: int, dropout_rate: float) -> None:
     super().__init__()
     self.d_model = d_model
+    self.self_attn = nn.MultiheadAttention(d_model, n_heads, dropout=dropout_rate, batch_first=True)
     self.dropout1 = nn.Dropout(dropout_rate)
     self.dropout2 = nn.Dropout(dropout_rate)
 
   def forward(self, embeddings: T.Tensor, padding_mask: T.Tensor) -> T.Tensor:
     """The decoder block takes in token embeddings and enriches them with a series
     of transfomations including causal self-attention."""
-    ...
+    causal_mask = make_causal_mask(padding_mask.shape[-1]).to('cuda')
+    self_attn_out, _ = self.self_attn(embeddings, embeddings, embeddings, key_padding_mask=padding_mask, attn_mask=causal_mask)
+    layer1 = F.layer_norm(self_attn_out + embeddings, normalized_shape=(self.d_model, ))
+    return layer1
 
   @classmethod
   def from_gptconfig(cls, config: gpt_config.GPTConfig) -> 'GPTDecoderBlock':
     return cls(config.d_model, config.d_k, config.d_v, config.n_attn_heads, config.dim_feedfwd, config.dropout_rate)
+
+def make_causal_mask(size: int) -> T.Tensor:
+  """Generate a causual attention mask from the input embeddings.
+  Args:
+    embeddings: BS * n_tokens * embedding_dim
+  Returns:
+    causal attention mask of shape (BS, n_tokens, embedding_dim)
+  """
+  return T.tril(T.ones(size, size))
--- gpt/gpt/toy_training.py
+++ gpt/gpt/toy_training.py
@@ -1,23 +1,63 @@
 """This is a toy training script to check that the model is
 training as expected."""
 
+from typing import NamedTuple, Sequence
 import random
+import torch as T
+from torch import optim
+import torch.nn.functional as F
+from torch.utils.data import Dataset, DataLoader
 
 from gpt import modeling, gpt_config
 
-def make_training_data():
+N_EPOCHS = 3
+END_TOKEN = 27
+PADDING_ID = 0
+BATCH_SIZE = 4
+
+class TrainingPoint(NamedTuple):
+  tokens: T.Tensor
+  padding_mask: T.Tensor
+
+def make_training_data(n_datapoints: int):
   """
-  The training task is to learn the alphabet, i.e.
-  given a sequence of characters, output the next letter in
-  the alphabet. E.g. 'ghi' -> 'j'.
-  We use the convention that after 'z', we wrap back around to 'a'.
+  The training task is to learn the alphabet,
+  I.e. w -> xyz
   """
-  ...
+  return [_make_training_datapoint() for _ in range(n_datapoints)]
+
+def _make_training_datapoint():
+  start_point = random.randint(0, 26)
+  tokens = T.Tensor(list(range(start_point, END_TOKEN + 1))).type(T.long)
+  return TrainingPoint(tokens, T.ones(len(tokens)))
+
+def collate_trainingpoints(trainingpoints: Sequence[TrainingPoint]) -> TrainingPoint:
+  tokens = [t.tokens for t in trainingpoints]
+  masks = [t.padding_mask for t in trainingpoints]
+  max_tokens = len(max(tokens, key=len))
+  batched_tokens = T.vstack([F.pad(input=t, pad=(0,max_tokens-len(t)), mode='constant', value=PADDING_ID) for t in tokens])
+  batched_mask = T.vstack([F.pad(input=m, pad=(0,max_tokens-len(m)), mode='constant', value=PADDING_ID) for m in masks])
+  return TrainingPoint(batched_tokens.type(T.long), batched_mask.type(T.bool))
 
 def main():
-  config = gpt_config.GPTConfig(26, 512, 1024, 32, 32, 32, 4, 4, 0.1)
+  config = gpt_config.GPTConfig(26+2, 512, 1024, 32, 32, 32, 4, 4, 0.1)
   model = modeling.GPT(config)
+  device = T.device("cuda" if T.cuda.is_available() else 'cpu')
+  model.to(device)
   print('number of parameters:', sum([p.numel() for p in model.parameters()]))
+
+  optimizer = optim.AdamW(model.parameters(), lr=1e-3)
+  data = make_training_data(10000)
+  dataloader = DataLoader(data, batch_size=BATCH_SIZE, collate_fn=collate_trainingpoints)
+  for _ in range(N_EPOCHS):
+    for batch in dataloader:
+      optimizer.zero_grad()
+      batch = [b.to(device) for b in batch]
+      _, loss = model(*batch)
+      loss.backward()
+      optimizer.step()
+      print(loss)
+
 
 if __name__ == "__main__":
   main()
